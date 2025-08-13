2c83824ce4141fe9d3a0eefb253e61d2aca74bc3
zh88168@yeah.net <zh88168@yeah.net>
2025-08-13 15:29:48 +0800

    增加测试心跳接口测试

--- loan_app/checkUser.py
+++ loan_app/checkUser.py
@@ -1,5 +1,5 @@
 
-from locust import HttpUser, task, tag
+from locust import HttpUser, task, tag, run_single_user
 
 '''
 只定义virtual user 继承自 HttpUser
@@ -16,7 +16,7 @@ class CheckUser(HttpUser):
                "Content-Type": "application/json",
                "User-Agent": "locust"}
 
-    @task
+    @task(0)
     @tag('check0')
     def check(self):
         for num in range(9100000000, 9100000099):
@@ -27,7 +27,7 @@ class CheckUser(HttpUser):
                 if res['success'] is False:
                     e.failure("失败")
                 e.success()
-    @task
+    @task(0)
     @tag('check1')
     def check2(self):
         for num in range(9100000000, 9100000099):
@@ -39,6 +39,19 @@ class CheckUser(HttpUser):
                     e.failure("失败")
                 e.success()
 
+    @task(10)
+    @tag(' heartBeat')
+    def heartBeat(self):
+        with self.client.get('/loanapp/HeartBeat/heartBeat', headers=self.headers, catch_response=True, name="heartBeat") as e:
+            res = e.json()
+            if res['success'] is False:
+                e.failure()
+            e.success()
+
     host = "http://app-fat.sandbox-shuangqiang.top"
     min_wait = 1000
     max_wait = 3000
+
+
+if __name__ == '__main__':
+    run_single_user(CheckUser, include_context=True, loglevel="INFO")
